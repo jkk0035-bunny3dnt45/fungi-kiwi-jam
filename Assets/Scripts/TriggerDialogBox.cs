99383a7f8266e0ccc7d6b99722ddf00c744aba5e
maybeken <ken.lam@grd0.net>
2026-07-26 11:19:57 +1200

    feat: Integrating the partical, flicker light, and sound effect

--- Assets/Scripts/TriggerDialogBox.cs
+++ Assets/Scripts/TriggerDialogBox.cs
@@ -1,26 +1,53 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 public class TriggerDialogBox : MonoBehaviour
 {
-    public string Tag = "Player"; //Only accepts colliders with the player tag
+    public string Tag = "Player";
+    [TextArea]
+    public string message;
 
+    private bool playerInTrigger = false;
+    private GUIStyle popupStyle;
+    private bool styleInitialized;
 
-    //Need to create a Dialog Controller in the UI of the player prefab to display dialog
     private void OnTriggerEnter(Collider other)
     {
-        //Need to include
+        if (other.gameObject.tag == Tag)
+        {
+            playerInTrigger = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //Need to include
+        if (other.gameObject.tag == Tag)
+        {
+            playerInTrigger = false;
+        }
     }
 
-
-    private void OnTriggerStay(Collider other)
+    private void OnGUI()
     {
-        //Need to include
+        if (!playerInTrigger) return;
+
+        if (!styleInitialized)
+        {
+            popupStyle = new GUIStyle(GUI.skin.box);
+            popupStyle.fontSize = 18;
+            popupStyle.alignment = TextAnchor.MiddleCenter;
+            popupStyle.wordWrap = true;
+            popupStyle.normal.textColor = Color.white;
+            styleInitialized = true;
+        }
+
+        float width = Screen.width * 0.4f;
+        float height = 100;
+        float x = (Screen.width - width) * 0.5f;
+        float y = Screen.height * 0.65f;
+
+        Rect popupRect = new Rect(x, y, width, height);
+
+        GUI.Box(popupRect, GUIContent.none, popupStyle);
+        GUI.Label(popupRect, message, popupStyle);
     }
 }
